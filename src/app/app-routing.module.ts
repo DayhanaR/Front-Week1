a401da22d2b39eaf98a592cfa5fefee74f395792
DayhanaRamirez <dayhanaramirez45@gmail.com>
2022-03-14 08:03:55 -0500

    Finished sidebar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UberComponent } from './components/uber/uber.component';
+import { PuntosColombiaComponent } from './components/puntos-colombia/puntos-colombia.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'uber', component : UberComponent},
+  {path: 'puntosColombia', component : PuntosColombiaComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,22 +6,24 @@ import { AppComponent } from './app.component';
 import { UberComponent } from './components/uber/uber.component';
 import { PuntosColombiaComponent } from './components/puntos-colombia/puntos-colombia.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
 
 //Material
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatListModule} from '@angular/material/list';
-import {SidenavComponent } from './components/sidenav/sidenav.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { LayoutModule } from '@angular/cdk/layout';
 
 @NgModule({
   declarations: [
     AppComponent,
     UberComponent,
     PuntosColombiaComponent,
-    SidenavComponent
+    SidebarComponent,
   ],
   imports: [
     BrowserModule,
@@ -32,7 +34,12 @@ import {MatDividerModule} from '@angular/material/divider';
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
-    MatDividerModule
+    MatDividerModule,
+    LayoutModule,
+    RouterModule.forRoot([
+      {path: 'uber', component: UberComponent},
+      {path: 'puntos-colombia', component: PuntosColombiaComponent},
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
